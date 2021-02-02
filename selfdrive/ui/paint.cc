fdbce10c1a1e47fb5d03c041b0c780d1d945e57b
jungch <jungch@naver.com>
2021-02-02 22:48:58 +0900

    Update paint.cc

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -174,7 +174,7 @@ static void ui_draw_line(UIState *s, const vertex_data *v, const int cnt, NVGcol
 
 static void update_track_data(UIState *s, const cereal::ModelDataV2::XYZTData::Reader &line, track_vertices_data *pvd) {
   const UIScene *scene = &s->scene;
-  const float off = 0.8; // Track line 80% compared to car
+  const float off = 1.0; // Track line 100% compared to car
   int max_idx = 0;
   float lead_d;
   if (s->sm->updated("radarState")) {
